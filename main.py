a2bacb87e15590a0b5784cda06866e002d64d49d
Thomas Runte <thomas@etnur.net>
2018-05-31 18:39:40 +1200

    recovered main.py removed .idea

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from NNLayer.util import print_matrix
 
 
 # TODO: input normalization
-
 # TODO: learn to plot functions
+# TODO: put on github
 # try to learn n-bit XOR
 # create all tuples:
 
@@ -26,9 +26,40 @@ default_params = {
     }
 
 
+# adaptive learning rate, so that
+# adapt_lr(alpha_max, alpha_min, i_max, 0)      = alpha_max
+# adapt_lr(alpha_max, alpha_min, i_max, i_max)  = alpha_min
+# idea: calculate a, b so that a/(b + i_curr) = alpha_max for i_curr = 0
+
+# cache values of b for tuples (alpha_max, alpha_min, i_max)
+alpha_params = {}
+
+
 def adapt_lr(alpha_max, alpha_min, i_max, i_curr):
-    b = - i_max / (1 - alpha_max/alpha_min)
+    if (alpha_max, alpha_min, i_max) in alpha_params:
+        b = alpha_params[(alpha_max, alpha_min, i_max)]
+    else:
+        alpha_params[(alpha_max, alpha_min, i_max)] = b = - i_max / (1 - alpha_max/alpha_min)
     return alpha_max * b / (i_curr + b)
+
+
+def test_adapt_alpha():
+    alpha_max = 0.1
+    alpha_min = 0.01
+    i_max = 10000
+    i_cur = 0
+
+    print(
+        "alpha max:" + "{:6.2f}".format(alpha_max) + " min:" + "{:6.2f}".format(alpha_min) + " i max:" + "{:6d}".format(
+            i_max) +
+        " curr:" + "{:6d}".format(i_cur) + " alpha:" + "{:8.4f}".format(adapt_lr(alpha_max, alpha_min, i_max, i_cur)))
+
+    for i in range(0, 21):
+        i_cur = int(i_max * i / 20)
+        print("alpha max:" + "{:6.2f}".format(alpha_max) + " min:" + "{:6.2f}".format(
+            alpha_min) + " i max:" + "{:6d}".format(i_max) +
+              " curr:" + "{:6d}".format(i_cur) + " alpha:" + "{:8.4f}".format(
+            adapt_lr(alpha_max, alpha_min, i_max, i_cur)))
 
 
 def init_soigt():
@@ -75,6 +106,22 @@ def init_soigt():
 
 
 def init_xor():
+
+    # Fuzzy XOR -> random float input X[i,j] in rand[0,1] -> y = XOR of (X>0.5)
+    # XOR is computed by extracting tmp = x[0] and then computing
+    # tmp = tmp != x[i] for i in {1,n}
+
+    def rand_data(n,m):
+        x = np.random.rand(n,m)
+        x_dig = (x > 0.5)
+        y = np.zeros((1,m))
+        for i in range(0, m):
+            y_tmp = x_dig[0, i]
+            for j in range(1, n):
+                y_tmp = y_tmp != x_dig[j, i]
+            y[0, i] = y_tmp * 1
+        return x,y
+
     np.random.seed(1)
 
     size = 4
@@ -102,33 +149,16 @@ def init_xor():
 
     print("topology:" + str(parameters["topology"]))
 
-    # Fuzzy XOR -> random float input X[i,j] in [0,1] -> y = XOR of (X>0.5)*1
 
-    x = np.random.rand(size,m)
-    x_dig = (x > 0.5)
-    y = np.zeros((1,m))
-    for i in range(0,m):
-        y_tmp = x_dig[0,i]
-        for j in range(1,size):
-            y_tmp = y_tmp != x_dig[j,i]
-        y[0,i] = y_tmp * 1
-
+    x, y = rand_data(size,m)
     parameters["X"] = x
     parameters["Y"] = y
 
-    m_t = int(m/5)
-    x = np.random.rand(size,m_t)
-    x_dig = (x > 0.5)
-    y = np.zeros((1, m_t))
-    for i in range(0,m_t):
-        y_tmp = x_dig[0,i]
-        for j in range(1,size):
-            y_tmp = y_tmp != x_dig[j,i]
-        y[0,i] = y_tmp * 1
-
+    x, y = rand_data(size, int(m/5))
     parameters["X_t"] = x
     parameters["Y_t"] = y
 
+    # this did not work at all
     #x = np.zeros((size,size * size))
     #y = np.zeros((1,size*size))
     #for i in range(0,size*size):
@@ -197,19 +227,9 @@ def learn(parameters):
     return network
 
 
+
+
+# test_adapt_alpha()
 learn(init_xor())
 # learn(init_soigt())
 
-# alpha_max = 0.1
-# alpha_min = 0.01
-# i_max = 10000
-# i_cur = 0
-
-
-# print("alpha max:" + "{:6.2f}".format(alpha_max) + " min:" + "{:6.2f}".format(alpha_min) + " i max:" + "{:6d}".format(i_max) +
-#       " curr:" + "{:6d}".format(i_cur) + " alpha:" + "{:8.4f}".format(adapt_lr(alpha_max,alpha_min,i_max,i_cur)))
-
-# for i in range(0,21):
-#    i_cur = int(i_max * i / 20
-#    print("alpha max:" + "{:6.2f}".format(alpha_max) + " min:" + "{:6.2f}".format(alpha_min) + " i max:" + "{:6d}".format(i_max) +
-#    " curr:" + "{:6d}".format(i_cur) + " alpha:" + "{:8.4f}".format(adapt_lr(alpha_max,alpha_min,i_max,i_cur)))
