found bug in y_hat calc, some optimization, prepare for gradient checking

NNToolkit/activation.py
@@ -53,12 +53,7 @@ class ReLU(Activation):
         return np.maximum(0, z)
 
     def get_grads(self, z, da):
-        if self.__ones is None:
-            self.__ones = np.ones(z.shape)
-        elif z.shape != self.__ones.shape:
-            self.__ones = np.ones(z.shape)
-
-        return np.multiply(self.__ones * (z > 0), da)
+        return np.multiply(np.int64(z > 0), da)
 
     def get_he_init(self,l_prev):
         return np.sqrt(2 / l_prev)

NNToolkit/layer.py
@@ -113,7 +113,9 @@ class Layer:
                 dw = np.dot(dz, a_in.T) / m
 
             db = np.sum(dz, axis=1, keepdims=True)/m
-            res["dA"] = np.dot(w.T, dz)
+
+            if self.__layer_idx > 1:
+                res["dA"] = np.dot(w.T, dz)
 
             if "verbose" in params:
                 print("dz[" + str(self.__layer_idx) + "]:" + print_matrix(dz, 6))
@@ -126,12 +128,16 @@ class Layer:
                 w = w - alpha * dw
                 b = b - alpha * db
 
-                if self.__local_params:
-                    self.__W = w
-                    self.__b = b
-                else:
-                    res["W" + str(self.__layer_idx)] = w
-                    res["b" + str(self.__layer_idx)] = b
+            if self.__local_params:
+                self.__W = w
+                self.__b = b
+
+            if not self.__local_params | ("dump_params" in params):
+                res["W" + str(self.__layer_idx)] = w
+                res["b" + str(self.__layer_idx)] = b
+                res["dW" + str(self.__layer_idx)] = dw
+                res["db" + str(self.__layer_idx)] = db
+
         return res
 
     def size(self):

NNToolkit/terminal.py
@@ -15,7 +15,11 @@ class Terminal(Layer):
 
     def process(self, a, params):
         y_hat = np.zeros(a.shape)
-        y_hat[np.where(a == np.max(a, axis=0))] = 1
+        if a.shape[0] > 1:
+            y_hat[np.where(a == np.max(a, axis=0))] = 1
+        else:
+            y_hat = np.int64(a > 0.5)
+
         res = {"Y_hat": y_hat}
         # a.argmax(axis=0)
         # y_hat = np.zeros(a.shape)

samples/handwriting.py
@@ -20,7 +20,7 @@ from NNToolkit.manage import evaluate
 def init_hand_writing():
     np.random.seed(1)
     matrix = scipy.io.loadmat("../data/mlnn.mat")
-    assert isinstance(matrix["X"],np.ndarray)
+    assert isinstance(matrix["X"], np.ndarray)
     assert isinstance(matrix["y"], np.ndarray)
 
     x_raw = matrix["X"]
@@ -35,21 +35,21 @@ def init_hand_writing():
     n_0 = x_raw.shape[1]
     n_l = 10
 
-    y_class = np.zeros((m,n_l))
+    y_class = np.zeros((m, n_l))
     for i in range(0,n_l):
         y_class[:,i:i+1] = (y_raw == (i + 1))
 
-    res = divide2sets(x_raw,y_class,0,0.01,True,True)
+    res = divide2sets(x_raw, y_class, 0, 0.01, True, True)
 
     print("shape: n0:" + str(n_0) + " nL:" + str(n_l) + " m:" + str(m))
 
     parameters = {"alpha": 0.5,
                   "alpha_min": 0.02,
-                  "lambda": 0.04,
+                  "lambda": 0.8,
                   "verbose": 0,
-                  "iterations": 1500,
+                  "iterations": 500,
                   "graph" : True,
-                  "topology": [n_0,300,200, n_l],
+                  "topology": [n_0, 300, 200, 100, n_l],
                   "activations": [act.ReLU,act.Sigmoid],  # [act.TanH, act.Sigmoid]
                   "X" : res["X_train"],
                   "Y" : res["Y_train"]
@@ -72,9 +72,9 @@ network = learn(params)
 network.get_weights(params)
 ts = "{:%Y%m%d-%H%M%S}".format(datetime.datetime.now())
 if "X_t" in params:
-    y_hat,acc = evaluate(network,params["X_t"],params["Y_t"])
+    y_hat,acc = evaluate(network, params["X_t"], params["Y_t"])
     acc_tag = "_" + "{:02d}".format(int(acc * 100))
 else:
     acc_tag = ""
 
-save_params(params,"../testCases/handWr_1000_t_92_" + ts + acc_tag + ".json.gz")
+save_params(params, "../testCases/handWr_1000_t_92_" + ts + acc_tag + ".json.gz")

samples/mean_gt.py
@@ -7,19 +7,21 @@ from NNToolkit.manage import evaluate
 from NNToolkit.util import save_params
 from NNToolkit.util import read_params
 
-def init_mean_gt(m = 1000):
+
+def init_mean_gt(m = 1000, it = 2000):
     np.random.seed(1)
 
     threshold = 20
     offset = 10
-    size = 5
+    size = 10
 
-    parameters = {"alpha": 0.03,
-                  "alpha_min": 0.005,
+    parameters = {"alpha": 0.01,
+                  #"alpha_min": 0.005,
                   "verbose": 0,
-                  "iterations": 10000,
-                  "epsilon": 0.005,
-                  "topology": [size,size, 1],
+                  "iterations": it,
+                  "lambda": 8,
+                  "graph" : True,
+                  "topology": [size,int(size/2),1],
                   "activations": [act.ReLU,act.Sigmoid]  # [act.TanH, act.Sigmoid]
                   }
 
@@ -36,10 +38,10 @@ def init_mean_gt(m = 1000):
 
     print("topology:" + str(parameters["topology"]))
 
-    parameters["X"] = x = np.random.randn(size,m) * threshold + offset
-    parameters["Y"] = ((np.sum(x,axis=0,keepdims=True) / size) > offset) * 1
-    parameters["X_t"] = x = np.random.randn(size, int(m/5)) * threshold + offset
-    parameters["Y_t"] = ((np.sum(x, axis=0, keepdims=True) / size) > offset) * 1
+    parameters["X"] = x = np.random.randn(size,m)
+    parameters["Y"] = np.int64((np.sum(x,axis=0,keepdims=True) / size) > 0)
+    parameters["X_t"] = x = np.random.randn(size, int(m/5))
+    parameters["Y_t"] = np.int64((np.sum(x, axis=0, keepdims=True) / size) > 0)
 
     if parameters["verbose"] > 1:
         print("X:  " + print_matrix(parameters["X"],4))
@@ -54,6 +56,7 @@ def train_gt_mean(n = 1000):
     network.get_weights(params)
     save_params(params, "../testCases/mean_gt_" + str(n) + ".json.gz")
 
+
 def restore_gt_mean(n = 1000):
     params = read_params("../testCases/mean_gt_" + str(n) + ".json.gz")
     network = create(params)
@@ -64,7 +67,7 @@ def restore_gt_mean(n = 1000):
     print("test accuracy:    " + str(acc*100) + "%")
 
 
-train_gt_mean()
+train_gt_mean(2000)
 
 # restore_gt_mean()
 